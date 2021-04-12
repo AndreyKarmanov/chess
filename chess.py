5e109c83ebe90127b667364d485f33bb55d58131
Rakulj <78745082+Rakulj@users.noreply.github.com>
2021-04-12 11:06:19 -0400

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,5 +1,26 @@
 #This is chess
 
+class Board:
+    pass
+
 class Pieces:
     def __init__(self, colour):
         self._colour = colour
+
+class Rook(Pieces):
+    pass
+
+class Horse(Pieces):
+    pass
+
+class Bishop(Pieces):
+    pass
+
+class King(Pieces):
+    pass
+
+class Queen(Pieces):
+    pass
+
+class Pawn(Pieces):
+    pass
